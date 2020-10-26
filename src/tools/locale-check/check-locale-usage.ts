07ddc2eb3fd3d99f266763fa5723c517097f32b6
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-26 10:09:57 +0100

    firs bits and pieces

---
+++ src/tools/locale-check/check-locale-usage.ts
@@ -0,0 +1,56 @@
+/* eslint-disable */
+import ts, {
+  SyntaxKind,
+  Node,
+  Identifier,
+  ImportDeclaration,
+  Token,
+  Expression,
+  VariableDeclaration,
+  PropertyAccessExpression,
+  PrivateIdentifier,
+} from 'typescript';
+import fs from 'fs';
+import path from 'path';
+
+const componentPath = path.join(__dirname, 'test.ts');
+
+const node = ts.createSourceFile(
+  'x.ts',
+  fs.readFileSync(componentPath, 'utf8'),
+  ts.ScriptTarget.Latest
+);
+
+type IdentifierUnion = Identifier | PrivateIdentifier;
+
+const localeIdentifiers: Map<
+  IdentifierUnion,
+  IdentifierUnion | undefined
+> = new Map();
+const visitors: Record<number, (child: any) => void> = {
+  [SyntaxKind.VariableDeclaration]: (child: VariableDeclaration) => {
+    if (child.initializer?.kind === SyntaxKind.PropertyAccessExpression) {
+      const initializer: PropertyAccessExpression = child.initializer as PropertyAccessExpression;
+      if (!localeIdentifiers.has(initializer.name)) {
+        localeIdentifiers.set(child.name, undefined);
+      }
+    }
+  },
+  [SyntaxKind.ImportDeclaration]: (child: ImportDeclaration) => {
+    if (
+      child.importClause?.name &&
+      (child.moduleSpecifier as any).text.indexOf('/locale') > -1
+    ) {
+      localeIdentifiers.set(child.importClause.name, undefined);
+    }
+  },
+};
+
+const childVisitor = (child: Node) => {
+  if (visitors[child.kind]) {
+    visitors[child.kind](child);
+  }
+  child.forEachChild(childVisitor);
+};
+
+node.forEachChild(childVisitor);
---
+++ src/tools/locale-check/test.ts
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import siteText from '../../locale';
+
+const text = siteText.positief_geteste_personen;
+const test = text.barchart_axis_titel;
+const ggdText = siteText.positief_geteste_personen_ggd.titel;
